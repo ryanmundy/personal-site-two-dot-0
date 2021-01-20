290788c30d89bb1a80a2f1329043795d17330f3f
Ryan Mundy <ryanmundy34@gmail.com>
2021-01-19 22:08:29 -0500

    adds class to only show portrait on home view

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  onHomeView: boolean = false;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((val) => {
+      if (this.router.url === '/dashboard/home') {
+        this.onHomeView = true;
+      } else {
+        this.onHomeView = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
