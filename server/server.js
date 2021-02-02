b662a3eb08b448f1cab8227f00712d7347cc2785
Ryan Mundy <ryanmundy34@gmail.com>
2021-02-02 18:32:52 -0500

    preparing for deployment

--- server/server.js
+++ server/server.js
@@ -11,40 +11,49 @@ app.use(bodyParser.urlencoded({
     extended: true,
 }));
 
-app.get('/api', (request, response) => {
-    console.log(`URL: ${request.url}`);
-    response.send({ message: 'Node.js and Express REST API' });
-});
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/personal-site-two'));
 
-app.post('/api/contact', (request, response) => {
-    let mailOpts, smtpTrans;
-    smtpTrans = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            user: GMAIL_USER,
-            pass: GMAIL_PASS
-        }
-    });
-    mailOpts = {
-        from: request.body.name + ' &lt;' + request.body.email + '&gt;',
-        to: GMAIL_USER,
-        subject: 'New message from contact form at ryanmundy.herokuapp.com',
-        text: `${request.body.name} (${request.body.email}) says: ${request.body.message}`
-    };
-    smtpTrans.sendMail(mailOpts, function (error, response) {
-        if (error) {
-            response.send('contact-failure');
-        }
-        else {
-            response.send('contact-success');
-        }
-    });
-})
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', { root: 'dist/personal-site-two/' }),
+);
 
-const server = app.listen(port, (error) => {
-    if (error) return console.log(`Error: ${error}`);
+app.listen(process.env.PORT || 8080);
 
-    console.log(`Server listening on port ${server.address().port}`);
-});
+// app.get('/api', (request, response) => {
+//     console.log(`URL: ${request.url}`);
+//     response.send({ message: 'Node.js and Express REST API' });
+// });
+
+// app.post('/api/contact', (request, response) => {
+//     let mailOpts, smtpTrans;
+//     smtpTrans = nodemailer.createTransport({
+//         host: 'smtp.gmail.com',
+//         port: 465,
+//         secure: true,
+//         auth: {
+//             user: GMAIL_USER,
+//             pass: GMAIL_PASS
+//         }
+//     });
+//     mailOpts = {
+//         from: request.body.name + ' &lt;' + request.body.email + '&gt;',
+//         to: GMAIL_USER,
+//         subject: 'New message from contact form at ryanmundy.herokuapp.com',
+//         text: `${request.body.name} (${request.body.email}) says: ${request.body.message}`
+//     };
+//     smtpTrans.sendMail(mailOpts, function (error, response) {
+//         if (error) {
+//             response.send('contact-failure');
+//         }
+//         else {
+//             response.send('contact-success');
+//         }
+//     });
+// })
+
+// const server = app.listen(port, (error) => {
+//     if (error) return console.log(`Error: ${error}`);
+
+//     console.log(`Server listening on port ${server.address().port}`);
+// });
