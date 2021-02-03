44de1d9b88fa28e1b13808dc88bf3d28d422e5a0
Ryan Mundy <ryanmundy34@gmail.com>
2021-02-02 19:14:44 -0500

    testing email

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 5000;
+const port = 8080;
 const app = express();
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -25,32 +25,32 @@ app.listen(process.env.PORT || 8080);
 //     response.send({ message: 'Node.js and Express REST API' });
 // });
 
-// app.post('/api/contact', (request, response) => {
-//     let mailOpts, smtpTrans;
-//     smtpTrans = nodemailer.createTransport({
-//         host: 'smtp.gmail.com',
-//         port: 465,
-//         secure: true,
-//         auth: {
-//             user: GMAIL_USER,
-//             pass: GMAIL_PASS
-//         }
-//     });
-//     mailOpts = {
-//         from: request.body.name + ' &lt;' + request.body.email + '&gt;',
-//         to: GMAIL_USER,
-//         subject: 'New message from contact form at ryanmundy.herokuapp.com',
-//         text: `${request.body.name} (${request.body.email}) says: ${request.body.message}`
-//     };
-//     smtpTrans.sendMail(mailOpts, function (error, response) {
-//         if (error) {
-//             response.send('contact-failure');
-//         }
-//         else {
-//             response.send('contact-success');
-//         }
-//     });
-// })
+app.post('/api/contact', (request, response) => {
+    let mailOpts, smtpTrans;
+    smtpTrans = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            user: GMAIL_USER,
+            pass: GMAIL_PASS
+        }
+    });
+    mailOpts = {
+        from: request.body.name + ' &lt;' + request.body.email + '&gt;',
+        to: GMAIL_USER,
+        subject: 'New message from contact form at ryanmundy.herokuapp.com',
+        text: `${request.body.name} (${request.body.email}) says: ${request.body.message}`
+    };
+    smtpTrans.sendMail(mailOpts, function (error, response) {
+        if (error) {
+            response.send('contact-failure');
+        }
+        else {
+            response.send('contact-success');
+        }
+    });
+})
 
 // const server = app.listen(port, (error) => {
 //     if (error) return console.log(`Error: ${error}`);
